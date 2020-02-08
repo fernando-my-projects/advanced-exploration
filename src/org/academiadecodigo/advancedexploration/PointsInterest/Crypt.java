49f554dc96d1ef6a116f436256377d69c0e9ddbc
André Pinho <andreseguradopinho@gmail.com>
2020-02-08 18:58:12 +0000

    Added Main, player methods and PIs properties and method

--- src/org/academiadecodigo/advancedexploration/PointsInterest/Crypt.java
+++ src/org/academiadecodigo/advancedexploration/PointsInterest/Crypt.java
@@ -1,4 +1,21 @@
 package org.academiadecodigo.advancedexploration.PointsInterest;
 
+import org.academiadecodigo.advancedexploration.Entities.Player;
+
 public class Crypt extends PointsInterest {
+
+    public Crypt(){
+        reward = 100;
+        penalty = -50;
+        risk = 0.7;
+    }
+
+    @Override
+    public void getExploreResult(Player player) {
+       if (Math.random() < risk){
+           player.setEnergy(penalty);
+           return;
+       } player.setPoints(reward);
+
+    }
 }
