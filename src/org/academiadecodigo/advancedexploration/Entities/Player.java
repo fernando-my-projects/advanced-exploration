816781c955651a703fa7ef1d9b2fcfaf38030721
André Pinho <andreseguradopinho@gmail.com>
2020-02-08 20:25:10 +0000

    added obstacle properties and interaction with player

--- src/org/academiadecodigo/advancedexploration/Entities/Player.java
+++ src/org/academiadecodigo/advancedexploration/Entities/Player.java
@@ -1,5 +1,6 @@
 package org.academiadecodigo.advancedexploration.Entities;
 
+import org.academiadecodigo.advancedexploration.Obstacles.Obstacle;
 import org.academiadecodigo.advancedexploration.PointsInterest.PointsInterest;
 import org.academiadecodigo.advancedexploration.Position;
 
@@ -8,6 +9,8 @@ public class Player {
     private int energy = 500;
     private int points = 0;
     private Position pos;
+    private boolean hasHat;
+    private boolean hasWhip;
 
     public Player(){
 
@@ -16,6 +19,18 @@ public class Player {
 
     public void explore(PointsInterest pi){
         pi.getExploreResult(this);
+    }
+
+    public void fight(Obstacle obs){
+        obs.getFightResult(this);
+    }
+
+    public void putOnHat(){
+        hasHat = true;
+    }
+
+    public void pickWhip(){
+        hasWhip = true;
     }
 
     public void setEnergy(int energy) {
--- src/org/academiadecodigo/advancedexploration/Obstacles/Obstacle.java
+++ src/org/academiadecodigo/advancedexploration/Obstacles/Obstacle.java
@@ -1,4 +1,18 @@
 package org.academiadecodigo.advancedexploration.Obstacles;
 
-public class Obstacle {
+import org.academiadecodigo.advancedexploration.Entities.Player;
+import org.academiadecodigo.advancedexploration.Position;
+
+public abstract class Obstacle {
+
+    protected int damage;
+    protected int reward;
+    protected Position pos;
+
+    public Obstacle(){
+        pos = new Position();
+    }
+
+    public abstract void getFightResult(Player player);
+
 }
