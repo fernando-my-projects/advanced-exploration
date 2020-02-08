4b603c37c03e2f5c910f7a10a1fc524866b8de1d
Fernando Amado <fernandopintoamado@outlook.pt>
2020-02-08 19:03:55 +0000

    Class FileAndMovements

---
+++ src/org/academiadecodigo/advancedexploration/pictures/ImagesAndMoviment.java
@@ -0,0 +1,21 @@
+package org.academiadecodigo.advancedexploration.pictures;
+
+import org.academiadecodigo.advancedexploration.keyboard.KeyboardHandler;
+
+public class ImagesAndMoviment implements KeyboardHandler {
+
+    private Picture cellImages;
+    private Picture playerImage;
+
+    public ImagesAndMoviment() {
+        //cellImages = new Picture(0,0)
+        playerImage = new Picture(100, 100, )
+    }
+
+
+
+
+
+
+
+}
