2797409f7a48e5490081f24c47bd1d58a3b665c9
André Pinho <andreseguradopinho@gmail.com>
2020-02-08 17:54:52 +0000

    added properties of field and player

--- src/org/academiadecodigo/advancedexploration/Field.java
+++ src/org/academiadecodigo/advancedexploration/Field.java
@@ -1,4 +1,15 @@
 package org.academiadecodigo.advancedexploration;
 
 public class Field {
+
+    private int width;
+    private int height;
+
+
+    public Field (int cols, int rows){
+        width = cols;
+        height = rows;
+    }
+
+
 }
