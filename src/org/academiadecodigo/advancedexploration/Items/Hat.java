56d0fb32a04c745950e36c334a445b777029fe0e
André Pinho <andreseguradopinho@gmail.com>
2020-02-09 17:58:57 +0000

    implemented property and method in items, hat, whip

--- src/org/academiadecodigo/advancedexploration/Items/Hat.java
+++ src/org/academiadecodigo/advancedexploration/Items/Hat.java
@@ -1,4 +1,11 @@
 package org.academiadecodigo.advancedexploration.Items;
 
+import org.academiadecodigo.advancedexploration.Entities.Player;
+
 public class Hat extends Item {
+
+    @Override
+    public void equip(Player player) {
+        player.putOnHat();
+    }
 }
